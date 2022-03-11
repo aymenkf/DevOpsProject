36de5ab46d03f9d664fe8f89270fe807be694cfc
unknown <ons.jouini@esprit.tn>
2022-03-11 23:55:09 +0100

    one

--- GesFormation/src/main/java/com/esprit/examen/GesFApplication.java
+++ GesFormation/src/main/java/com/esprit/examen/GesFApplication.java
@@ -1,11 +1,12 @@
 package com.esprit.examen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 
 @SpringBootApplication
-public class GesFApplication {
+public class GesFApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GesFApplication.class, args);
---
+++ GesFormation/src/test/java/com/esprit/examen/controllers/SessionRestControllerTest.java
@@ -0,0 +1,33 @@
+package com.esprit.examen.controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import lombok.extern.slf4j.Slf4j;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class SessionRestControllerTest {
+/*	@Test
+	public void testAjouterSession() {
+		log.info("********************************Start Method Test Add Contract ******************************************************");
+		System.out.println("sdfsfdf");
+	}
+
+	@Test
+	public void testModifierSession() {
+	}
+
+	@Test
+	public void testAffecterFormateurASession() {
+	}
+
+	@Test
+	public void testSupprimerSession() {
+	}
+*/
+}
